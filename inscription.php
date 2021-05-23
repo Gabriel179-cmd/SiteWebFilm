83917d888ff85e60a79ce06a7f1027e9cf1e390d
Gabriel179-cmd <83180731+Gabriel179-cmd@users.noreply.github.com>
2021-05-23 21:46:30 +0200

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,37 +1,12 @@
-<?php
-// on teste si le visiteur a soumis le formulaire
-if (isset($_POST['inscription']) && $_POST['inscription'] == 'Inscription') {
-	// on teste l'existence de nos variables. On teste également si elles ne sont pas vides
-	if ((isset($_POST['login']) && !empty($_POST['login'])) && (isset($_POST['pass']) && !empty($_POST['pass'])) && (isset($_POST['pass_confirm']) && !empty($_POST['pass_confirm']))) {
-	// on teste les deux mots de passe
-	if ($_POST['pass'] != $_POST['pass_confirm']) {
-		$erreur = 'Les 2 mots de passe sont différents.';
-	}
-	else {
-		$base = mysql_connect ('serveur', 'login', 'password');
-		mysql_select_db ('nom_base', $base);
+<?php 
+// Vérification de la validité des informations
 
-		// on recherche si ce login est déjà utilisé par un autre membre
-		$sql = 'SELECT count(*) FROM membre WHERE login="'.mysql_escape_string($_POST['login']).'"';
-		$req = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
-		$data = mysql_fetch_array($req);
+// Hachage du mot de passe
+$pass_hache = password_hash($_POST['pass'], PASSWORD_DEFAULT);
 
-		if ($data[0] == 0) {
-		$sql = 'INSERT INTO membre VALUES("", "'.mysql_escape_string($_POST['login']).'", "'.mysql_escape_string(md5($_POST['pass'])).'")';
-		mysql_query($sql) or die('Erreur SQL !'.$sql.'<br />'.mysql_error());
-
-		session_start();
-		$_SESSION['login'] = $_POST['login'];
-		header('Location: membre.php');
-		exit();
-		}
-		else {
-		$erreur = 'Un membre possède déjà ce login.';
-		}
-	}
-	}
-	else {
-	$erreur = 'Au moins un des champs est vide.';
-	}
-}
-?>
+// Insertion
+$req = $bdd->prepare('INSERT INTO membres(pseudo, pass, email, date_inscription) VALUES(:pseudo, :pass, :email, CURDATE())');
+$req->execute(array(
+    'pseudo' => $pseudo,
+    'pass' => $pass_hache,
+    'email' => $email));
